19ce1d5648deebdc3acfec2f299dbf16c2c4ffe1
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-25 20:46:20 -0300

    feat: add withdraw call and changed deposit view

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -9,7 +9,15 @@ defmodule RocketpayWeb.AccountsController do
     with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
       conn
       |> put_status(:ok)
-      |> render("update.json", account: account)
+      |> render("deposit.json", account: account)
+    end
+  end
+
+  def withdraw(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
+      conn
+      |> put_status(:ok)
+      |> render("withdraw.json", account: account)
     end
   end
 end
