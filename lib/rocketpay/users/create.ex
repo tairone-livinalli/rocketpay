22d6dfbf6f7d8209704f39dfb921b87f3e13773f
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-25 11:19:18 -0300

    refactor: identation

--- lib/rocketpay/users/create.ex
+++ lib/rocketpay/users/create.ex
@@ -5,7 +5,7 @@ defmodule Rocketpay.Users.Create do
   def call(params) do
     Multi.new()
     |> Multi.insert(:create_user, User.changeset(params))
-    |> Multi.run(:create_account, fn repo, %{create_user: user} ->  insert_account(repo, user) end)
+    |> Multi.run(:create_account, fn repo, %{create_user: user} -> insert_account(repo, user) end)
     |> Multi.run(:preload_data, fn repo, %{create_user: user} -> preload_data(repo, user) end)
     |> run_transaction()
   end
