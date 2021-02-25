634e5e5ac32a55958c7894872f46e8bdf893bf99
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-25 20:47:07 -0300

    feat: add withdraw view and changed deposit view name

--- lib/rocketpay_web/views/accounts_view.ex
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -1,9 +1,19 @@
 defmodule RocketpayWeb.AccountsView do
   alias Rocketpay.{Account}
 
-  def render("update.json", %{account: %Account{id: account_id, balance: balance}}) do
+  def render("deposit.json", %{account: %Account{id: account_id, balance: balance}}) do
     %{
       message: "Deposit done successfully.",
+      account: %{
+        id: account_id,
+        balance: balance
+      }
+    }
+  end
+
+  def render("withdraw.json", %{account: %Account{id: account_id, balance: balance}}) do
+    %{
+      message: "Withdraw done successfully.",
       account: %{
         id: account_id,
         balance: balance
