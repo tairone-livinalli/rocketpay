80ec3fd54da8947eb5fe925a8bf8a149b34b6ab5
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2021-02-25 23:26:42 -0300

    feat: add accounts transaction view

--- lib/rocketpay_web/views/accounts_view.ex
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -1,5 +1,6 @@
 defmodule RocketpayWeb.AccountsView do
   alias Rocketpay.{Account}
+  alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
 
   def render("deposit.json", %{account: %Account{id: account_id, balance: balance}}) do
     %{
@@ -17,6 +18,27 @@ defmodule RocketpayWeb.AccountsView do
       account: %{
         id: account_id,
         balance: balance
+      }
+    }
+  end
+
+  def render("transaction.json", %{
+        transaction: %TransactionResponse{
+          origin_account: %Account{id: origin_id, balance: origin_balance},
+          destination_account: %Account{id: destination_id, balance: destination_balance}
+        }
+      }) do
+    %{
+      message: "Transaction done successfully.",
+      transaction: %{
+        origin_account: %{
+          id: origin_id,
+          balance: origin_balance
+        },
+        destination_account: %{
+          id: destination_id,
+          balance: destination_balance
+        }
       }
     }
   end
